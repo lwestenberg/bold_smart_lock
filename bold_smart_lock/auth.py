529c64da8d95c8d38235ffcc567923c01d4496fb
Lauren Westenberg <17823076+lwestenberg@users.noreply.github.com>
2022-10-24 20:45:29 +0200

    update github action, use standard AbstractAuth

--- bold_smart_lock/auth.py
+++ bold_smart_lock/auth.py
@@ -2,63 +2,33 @@
 
 from __future__ import annotations
 
-import logging
 from abc import ABC, abstractmethod
-from typing import Any, Mapping, Optional
-from aiohttp import ClientError, ClientResponse, ClientSession
-
-_LOGGER = logging.getLogger(__name__)
-
-HTTP_UNAUTHORIZED = 401
-AUTHORIZATION_HEADER = "Authorization"
-ERROR = "error"
-STATUS = "status"
-MESSAGE = "message"
+from aiohttp import ClientResponse, ClientSession
 
 class AbstractAuth(ABC):
     """Abstract class to make authenticated requests."""
 
     def __init__(self, websession: ClientSession, host: str):
         """Initialize the auth."""
-        self._websession = websession
-        self._host = host
+        self.websession = websession
+        self.host = host
 
     @abstractmethod
     async def async_get_access_token(self) -> str:
         """Return a valid access token."""
 
-    async def request(
-            self, method: str, url: str, **kwargs: Optional[Mapping[str, Any]]
-        ) -> ClientResponse:
-            """Make a request."""
-            headers = kwargs.get("headers")
+    async def request(self, method, url, **kwargs) -> ClientResponse:
+        """Make a request."""
+        headers = kwargs.get("headers")
 
-            if headers is None:
-                headers = {}
-            else:
-                headers = dict(headers)
-                del kwargs["headers"]
-            if AUTHORIZATION_HEADER not in headers:
-                try:
-                    access_token = await self.async_get_access_token()
-                except ClientError as err:
-                    print("error")
-                    # raise AuthException(f"Access token failure: {err}") from err
-                headers[AUTHORIZATION_HEADER] = f"Bearer {access_token}"
-            if not (url.startswith("http://") or url.startswith("https://")):
-                url = f"{self._host}/{url}"
-            _LOGGER.debug("request[%s]=%s", method, url)
-            if method == "post" and "json" in kwargs:
-                _LOGGER.debug("request[post json]=%s", kwargs["json"])
-            return await self._websession.request(method, url, **kwargs, headers=headers)
+        if headers is None:
+            headers = {}
+        else:
+            headers = dict(headers)
 
-    async def get(
-        self, url: str, **kwargs: Mapping[str, Any]
-    ) -> ClientResponse:
-        """Make a get request."""
-        try:
-            resp = await self.request("get", url, **kwargs)
-        except ClientError as err:
-            # raise ApiException(f"Error connecting to API: {err}") from err
-            print("error")
-        return await AbstractAuth._raise_for_status(resp)
+        access_token = await self.async_get_access_token()
+        headers["authorization"] = f"Bearer {access_token}"
+
+        return await self.websession.request(
+            method, f"{self.host}/{url}", **kwargs, headers=headers,
+        )
